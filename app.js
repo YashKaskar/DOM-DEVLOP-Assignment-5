1148c59abe8bbd6ba70332025a8ecdd904e180c2
Yash Kaskar <yash.kaskar99@gmail.com>
2022-10-12 11:21:24 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// Webiste Name: [Vercel](https://vercel.com/)
+// ### Tasks
+
+//       change the heading “Start with the developer” to “Start with Scratch”
+
+
+
+
+// Write your code here:
+
+document.querySelector('.section-title_title__VEDfK');
+<h4 class="section-title_title__VEDfK">​Start with the developer​</h4>​
+
+document.querySelector('.section-title_title__VEDfK').innerHTML = 'Start with Scratch';
+'Start with Scratch'
